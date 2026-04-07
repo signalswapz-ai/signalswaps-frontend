cedb72256e6e956b732b9d5c7b845d1ac59d2b8b
signalswapz-ai <signalswapz@gmail.com>
2026-04-07 16:36:00 -0400

    api end point is updated

--- src/app/service/apiservice.ts
+++ src/app/service/apiservice.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Apiservice {
-  private baseUrl = 'https://uniswap-api.onrender.com/api';
+  private baseUrl = 'https://signalswaps-v1.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
