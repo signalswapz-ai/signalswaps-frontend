c2285eee74fcc97d9f659eccbda10effc876dca1
signalswapz-ai <signalswapz@gmail.com>
2026-04-22 15:27:39 -0400

    feat:spot trade LIST Added

--- src/app/pages/uikit/ai-trade-history/ai-trade-history.ts
+++ src/app/pages/uikit/ai-trade-history/ai-trade-history.ts
@@ -2,21 +2,35 @@ import { AItrading } from '@/pages/service/aitrading';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { TagModule } from 'primeng/tag';
 
 
 @Component({
   selector: 'app-ai-trade-history',
   templateUrl: './ai-trade-history.html',
   styleUrl: './ai-trade-history.scss',
-  imports: [CommonModule, TableModule]
+  imports: [CommonModule, TableModule, DialogModule,TagModule]
 })
 export class AiTradeHistory {
   AItradeHistory: any[] = [];
   email: string = '';
+  // detailed card
+  displayTradeDialog = false;
+  selectedTrade: any = null;
+
 
   constructor(private aitrading: AItrading) { }
   ngOnInit() {
     this.getAItradeHistory();
+  }
+  openTradeDialog(trade: any) {
+    this.selectedTrade = trade;
+    this.displayTradeDialog = true;
+  }
+  closeTradeDialog() {
+    this.displayTradeDialog = false;
+    this.selectedTrade = null;
   }
 
   fetchUserUserData() {
