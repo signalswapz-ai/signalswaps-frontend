6d5d5b24d754889911603e69e447ffef51aee99a
signalswapz-ai <signalswapz@gmail.com>
2026-04-25 11:16:38 -0400

    feat:hero widget

--- src/app/pages/landing/components/highlightswidget.ts
+++ src/app/pages/landing/components/highlightswidget.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'highlights-widget',
-    templateUrl: './highlightswidget.html'
+    templateUrl: './highlightswidget.html',
+    styleUrls: ['./highlightswidget.scss']
 
 })
 export class HighlightsWidget {}
